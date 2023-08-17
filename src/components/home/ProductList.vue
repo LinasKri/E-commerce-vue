6152fb0d5f25540418691bd6c83c801f14100e5a
Linas Kriščiukaitis <l.krisciukaitis@gmail.com>
2023-08-17 11:17:09 +0300

    modified card img sizes

--- src/components/home/ProductList.vue
+++ src/components/home/ProductList.vue
@@ -5,51 +5,48 @@
         v-if="products.length"
         class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center"
       >
-        <div v-for="product in products" :key="product.id">
-          <div class="col mb-5">
-            <div class="card h-100">
-              <!-- Sale badge-->
-              <div
-                class="badge bg-dark text-white position-absolute"
-                style="top: 0.5rem; right: 0.5rem"
-              >
-                Sale
-              </div>
-              <!-- Product image-->
+        <div
+          v-for="product in products"
+          :key="product.id"
+          class="col mb-5 h-100"
+        >
+          <div class="card h-100">
+            <!-- ... existing code ... -->
+
+            <!-- Product image-->
+            <div class="product-image-container">
               <img
-                class="card-img-top"
+                class="card-img-top p-2"
                 :src="product.image"
                 :alt="product.title"
               />
-              <!-- Product details-->
-              <div class="card-body p-4">
-                <div class="text-center">
-                  <!-- Product name-->
-                  <h5 class="fw-bolder">{{ product.title }}</h5>
-                  <!-- Product reviews-->
-                  <div
-                    class="d-flex justify-content-center small text-warning mb-2"
-                  >
-                    <div class="bi-star-fill"></div>
-                    <div class="bi-star-fill"></div>
-                    <div class="bi-star-fill"></div>
-                    <div class="bi-star-fill"></div>
-                    <div class="bi-star-fill"></div>
-                  </div>
-                  <!-- Product price-->
-                  <span class="text-muted text-decoration-line-through"
-                    >${{ product.price }}</span
-                  >
-                  ${{ product.price }}
+            </div>
+            <!-- Product details-->
+            <div class="card-body p-4">
+              <div class="text-center">
+                <!-- Product name-->
+                <h5 class="fw-bolder text-truncate">{{ product.title }}</h5>
+                <!-- Product reviews-->
+                <div
+                  class="d-flex justify-content-center small text-warning mb-2"
+                >
+                  <div class="bi-star-fill"></div>
+                  <div class="bi-star-fill"></div>
+                  <div class="bi-star-fill"></div>
+                  <div class="bi-star-half"></div>
+                  <div class="bi-star"></div>
                 </div>
+                <!-- Product price-->
+                <span class="text-muted text-decoration-line-through"
+                  >${{ product.price }}</span
+                >
+                ${{ product.price }}
               </div>
-              <!-- Product actions-->
-              <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                <div class="text-center">
-                  <a class="btn btn-outline-dark mt-auto" href="#"
-                    >Add to cart</a
-                  >
-                </div>
+            </div>
+            <!-- Product actions-->
+            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+              <div class="text-center">
+                <a class="btn btn-outline-dark mt-auto" href="#">Add to cart</a>
               </div>
             </div>
           </div>
@@ -78,3 +75,14 @@ onMounted(() => {
   return { products };
 });
 </script>
+
+<style>
+.product-image-container {
+  height: 16rem;
+  overflow: hidden;
+}
+.card-img-top {
+  height: 100%;
+  object-fit: scale-down;
+}
+</style>
